46ec13fc9e8c113c499a0d8efb947d099c919bd7
Lucas Carvalho <141284703+luckymoura01@users.noreply.github.com>
2025-04-04 10:39:44 -0300

    Atualização V7.5
    Responsividade do cabeçalho do site

--- js/melhorias.js
+++ js/melhorias.js
@@ -9,6 +9,19 @@ function esconderFormulario() {
   botao.textContent = formulario.classList.contains("hidden") ? "<" : ">";
 }
 
+var SPMaskBehavior = function (val) {
+  return val.replace(/\D/g, '').length === 11 
+         ? '(00) 00000-0000' 
+         : '(00) 0000-00009';
+},
+spOptions = {
+  onKeyPress: function(val, e, field, options) {
+    field.mask(SPMaskBehavior.apply({}, arguments), options);
+  }
+};
+
+$('#telefone').mask(SPMaskBehavior, spOptions);
+
   function redirectToWhatsapp(event) {
     event.preventDefault();
 
